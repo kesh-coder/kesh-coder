fa5b1e97ff1ba8ba8224ef94e50a1d07be3c9965
kesh-coder <83479975+kesh-coder@users.noreply.github.com>
2021-05-06 11:40:56 +0530

    Create variable_in_c.c

---
+++ .github/workflows/variable_in_c.c
@@ -0,0 +1,13 @@
+ #include <iostream>
+using namespace std;
+
+int main(){
+	int one;
+        int two;
+        one= 1;
+        two= 2;
+
+        printf("one = %d \n",one);
+        printf("two = %d \n", two);
+	return 0;
+	}

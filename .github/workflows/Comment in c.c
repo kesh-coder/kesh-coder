f94615004f87d24420c3b1cfbce347da08a37206
kesh-coder <83479975+kesh-coder@users.noreply.github.com>
2021-05-06 11:34:32 +0530

    Create Comment in c.c

---
+++ .github/workflows/Comment in c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+using namespace std;
+
+int main(){
+//thus is single line comment
+/*this 
+is
+multi-line
+comment*/
+	Printf("comment not will print in output");
+	return 0;
+	}

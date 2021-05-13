cf7a9e4f93f1919a7d4244bb97ed754fa087cc12
kesh-coder <83479975+kesh-coder@users.noreply.github.com>
2021-05-13 17:02:17 +0530

    Add files via upload

---
+++ .github/workflows/11.oprators.c
@@ -0,0 +1,27 @@
+﻿
+
+#include<stdio.h>
+
+int main()
+{
+
+    int a, b, add, subtract, multiply, remainder;
+    float divide;
+
+    printf("Enter two integers: \n");
+    scanf("%d%d", &a, &b);
+
+    add = a+b;
+    subtract = a-b;
+    multiply = a*b;
+    divide = a/(float)b;
+    remainder = a%b;
+
+    printf("\n\nAddition of the numbers = %d\n", add);
+    printf("\nSubtraction of 2nd number from 1st = %d\n", subtract);
+    printf("\nMultiplication of the numbers = %d\n", multiply);
+    printf("\nDividing 1st number from 2nd = %f\n", divide);
+    printf("\nRemainder on Dividing 1st number by 2nd is %d\n", remainder);
+    printf("\n\n\t\t\tkeshav");
+    return 0;
+}

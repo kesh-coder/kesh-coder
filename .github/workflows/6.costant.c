1ed18a7034efc72dace49af8c5b312192a02fce0
kesh-coder <83479975+kesh-coder@users.noreply.github.com>
2021-05-13 17:26:16 +0530

    Add files via upload

---
+++ .github/workflows/6.costant.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(){
+	//float costant
+	float const pi = 3.14;
+	
+	//integer 
+	int const a = 5;
+	
+	//character
+	char const yes='y';
+	
+	printf("float = %f\n",pi);
+	printf("int = %d\n",a);
+	printf("char = %c\n",yes);
+	return 0;
+	}

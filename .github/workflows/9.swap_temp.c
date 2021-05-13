e5bca1ec43fe86dcabdf9cb41a268776899fa808
kesh-coder <83479975+kesh-coder@users.noreply.github.com>
2021-05-13 16:59:30 +0530

    Add files via upload

---
+++ .github/workflows/9.swap_temp.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+
+int main (){
+	int a,b;
+	int temp;
+	printf("write two numbers\n");
+	
+	scanf("%d",&a);
+	printf("a= %d\n",a);
+	
+	scanf("%d",&b);
+	printf("b= %d\n",b);
+	
+	printf("after swaping\n");
+	temp=a;
+	a=b;
+	b=temp;
+	printf("a= %d\nb= %d",a,b);
+return 0;
+}

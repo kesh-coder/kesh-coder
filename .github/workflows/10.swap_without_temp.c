6a29694d3e246eed2c02bf8a946f4da9beb3852c
kesh-coder <83479975+kesh-coder@users.noreply.github.com>
2021-05-13 17:00:06 +0530

    Add files via upload

---
+++ .github/workflows/10.swap_without_temp.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+int main()
+{ 
+    int first,second;
+    printf("Enter first integer value \n");
+    scanf("%d",&first);
+    printf("Enter second integer value \n");
+    scanf("%d",&second);
+    
+    first=first-second;
+    second=first+second;
+    first=second-first;
+    
+    printf("\nAfter swapping value of first integer is %d \nsecond integer is %d",first,second);
+    return 0;
+}

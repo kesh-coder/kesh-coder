2cd52d6cdf3c6153a22ec0554e17903530b248a3
kesh-coder <83479975+kesh-coder@users.noreply.github.com>
2021-07-09 09:36:50 +0530

    Create 27.for_loop & while_loop.c

---
+++ .github/workflows/27.for_loop & while_loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+  
+  int n=10;
+  int j=0;
+  for(int i=0; i<n; i++)
+    printf("%d",i);
+  
+  printf("ok\n");
+  
+  while(j<n){
+    printf("%d",j);
+    j++;
+    
+  }
+  
+ return 0; 
+}

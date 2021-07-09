1d3e072b45a09b2be534d469e15d312fd0f039c2
kesh-coder <83479975+kesh-coder@users.noreply.github.com>
2021-07-09 05:36:47 +0530

    Create 14.assignment operators.c

---
+++ .github/workflows/14.assignment operators.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ 
+ int main(){
+     int a=10;
+     int b=20;
+     int c=30;
+     int d=5;
+     int e=25;
+     int f=35;
+    
+    a+=b;
+    c-=d;
+    e*=f;
+    b/=10;
+    
+    printf("%i %i %i %i %i %i " ,a,b,c,d,e,f);
+ return 0;
+ }

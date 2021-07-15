7cfe3ebb3ed9eed34c3156de4ae85bb9c192f858
kesh-coder <83479975+kesh-coder@users.noreply.github.com>
2021-07-15 15:58:15 +0530

    Create 38.call by value.c

---
+++ .github/workflows/38.call by value.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int funct(int n){
+    int m=20;
+    n=m;
+    return n;
+}
+int main(){
+    int n=30;
+    printf("%d\n", funct(n));
+    printf("%d\n", n);
+
+    
+    return 0;
+}
